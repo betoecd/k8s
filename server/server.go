8b1890f5552d2373184f846b36e360451c7da0dc
Eberton Dias <eberton.bits@gmail.com>
2024-04-20 17:19:32 -0300

    feat: secrets and probs

--- server/server.go
+++ server/server.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
+var startedAt = time.Now()
+
 func main() {
+	http.HandleFunc("/health", Health)
+	http.HandleFunc("/configmap", ConfigMap)
+	http.HandleFunc("/secret", Secret)
 	http.HandleFunc("/", Home)
 	http.ListenAndServe(":8000", nil)
 }
@@ -17,4 +24,33 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	age := os.Getenv("AGE")
 
 	fmt.Fprintf(w, "Hello, I'm %s. I'm %s years old.", name, age)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	data, err := os.ReadFile("/config/NAME")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Fprint(w, string(data))
+
+}
+
+func Secret(w http.ResponseWriter, r *http.Request) {
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+
+	fmt.Fprintf(w, "User: %s\nPassword: %s", user, password)
+}
+
+func Health(w http.ResponseWriter, r *http.Request) {
+	uptime := time.Since(startedAt)
+
+	if uptime.Seconds() > 25 {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(fmt.Sprintf("Uptime: %s", uptime)))
+	} else {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	}
 }
